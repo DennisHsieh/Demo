f8788c361f1f495dbb05829bf8f6c0bfe1250027
DennisHsieh <denny0530@gmail.com>
2020-09-18 15:23:24 +0800

    Change long type to int for all value and function

--- funFibonacci.c
+++ funFibonacci.c
@@ -2,32 +2,32 @@
 #include <stdio.h>
   // (Finished) Aim:Tell ppl what is Fibonacci like, and ask for input n for the n-th value of the Fibonacci series.
   // (Finished) Aim2: Print all nubmer from 1st to n-th of the Fibonacci series.
-// Aim3: Overcome long restriction which is related to Aim4
+// Aim3: Overcome int restriction which is related to Aim4
 // Aim4: Too Slow, there must be some theory I don't know causing slow.
-long Fibonacci(long);
-long printAllFibonacci(long);
-long n;
+int Fibonacci(int);
+int printAllFibonacci(int);
+int n;
 int main(){
   printf("Fibonacci series is like 1,1,2,3,5,...\n");
   printf("Input n:= ");
-  scanf("%ld",&n);
-  printf("n is better less than 45 due to long type\n");
-  printf("The %ld-th number in Fibonacci series = %ld\n",n,Fibonacci(n));
-  printf("All number from 1 to %ld-th number in Fibonnaci series are ",n);
+  scanf("%d",&n);
+  printf("n is better less than 45 due to int type\n");
+  printf("The %d-th number in Fibonacci series = %d\n",n,Fibonacci(n));
+  printf("All number from 1 to %d-th number in Fibonnaci series are ",n);
   printAllFibonacci(n);
   return 0;
 }
-long Fibonacci(long n){
+int Fibonacci(int n){
   if ( n==1 || n==2){
     return 1;
   }
   return Fibonacci(n-1)+Fibonacci(n-2);
 }
-long printAllFibonacci(long n){
+int printAllFibonacci(int n){
 
-  for (long a=1;a<n;a++){
-      printf("%ld,",Fibonacci(a));
+  for (int a=1;a<n;a++){
+      printf("%d,",Fibonacci(a));
   }
-  printf("%ld\n",Fibonacci(n));
+  printf("%d\n",Fibonacci(n));
   return 0;
 }
