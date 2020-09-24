55f933b3748013ac754a734858e416d6b7d36f7a
DennisHsieh <denny0530@gmail.com>
2020-09-24 14:33:05 +0800

    Divide Factorial.h for better using. And test run testFactorial calling Factorial.h success.

---
+++ Factorial.h
@@ -0,0 +1,6 @@
+long Factorial(long n){
+  if (n==1){
+    return 1;
+  }
+  return n*Factorial(n-1);
+}
--- testFactorial.c
+++ testFactorial.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
-
+#include "Factorial.h"
 long n;
-long Factorial(long);
 int main(){
   printf("Input your number n:=");
   scanf("%ld",&n);
   // printf("\n");
   printf("%ld! = %ld\n",n,Factorial(n));
   return 0;
-}
-
-long Factorial(long n){
-  if (n==1){
-    return 1;
-  }
-  return n*Factorial(n-1);
 }
