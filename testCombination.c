96fb10b64675c4639772179fc27ac9b52652171f
DennisHsieh <denny0530@gmail.com>
2020-09-24 14:16:56 +0800

    Add irrational number testing and Combination calculating

---
+++ testCombination.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+  long m,n;
+  printf("Please input m, n to get combination of m choose n");
+  scanf("%ld,%ld", &m, &n);
+}
+long Combination(long m, long n){
+
+}
