d21bb860e4967b8ed37a254a0b2645bdbc298b10
DennisHsieh <denny0530@gmail.com>
2020-09-24 17:14:39 +0800

    Comment system(pause)

--- testCombination.c
+++ testCombination.c
@@ -6,6 +6,4 @@ int main(){
   printf("Please input m, n to get combination of m choose n");
   scanf("%ld %ld", &m, &n);
   printf("Combination of m choose n is = %ld", Combination(m,n));
-
-  // system("pause");
 }
