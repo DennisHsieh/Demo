a4026953e5a104479a8391d900768d925cba9855
DennisHsieh <denny0530@gmail.com>
2020-09-30 17:55:58 +0800

    Revised Input popup and output appearance

--- testTowerofHanoi.c
+++ testTowerofHanoi.c
@@ -3,15 +3,15 @@
 void cmove(int ,char,char,char);
 int main(void){
 	int n;
+  printf("Input n=");
 	scanf("%d",&n);
 	cmove(n,'a','b','c');
-	system("pause");
 }
 void cmove (int n,char a,char b, char c){
 	static int times=0;
 	if(n==1){
 		times++;
-	printf("%d times:Move %d from tower %c to %c\n",times,1,a,c);
+	printf("%d times: Move %d from tower %c to %c\n",times,1,a,c);
 	}
 	else{
 	cmove(n-1,a,c,b);
