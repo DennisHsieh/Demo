45d47de817d699b77dae36092d289e0f2d79276a
DennisHsieh <denny0530@gmail.com>
2020-10-05 18:11:54 +0800

    Remove desired function InputValueB to make it work

--- testMoraVerbose.c
+++ testMoraVerbose.c
@@ -4,7 +4,6 @@
 // Construct method s.t. f(a)>f(b), f(b)>f(c), f(c)>f(a);
 // Aim1: 1 time guessing.
 // Aim2: Try to make it interactive!
-int InputValueB(int, int []);
 void Mora(int [], int []);
 int n;
 int main(){
@@ -14,18 +13,16 @@ int main(){
   int A[n],B[n];
   for(int i=0;i<n;i++){
     A[i]=rand()%3-1;
-    InputValueB(i,B);
+    printf("Input B[%d] = ",i);
+    scanf("%d",&B[i]);
     while (B[i]>1 || B[i]<-1){
       printf("Input error, please retype like 1, 0 or -1\n");
-      InputValueB(i);
+      printf("Input B[%d] = ",i);
+      scanf("%d",&B[i]);
     }
   }
   printf("Computer has been prepared.");
   Mora(A,B);
-}
-int InputValueB(int i, int B[]){
-  printf("Input B[%d] = ",i);
-  scanf("%d",&B[i]);
 }
 void Mora(int A[], int B[]){
 
