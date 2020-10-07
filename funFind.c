db50ee50b1b578f412b743cd968e3251a1b41ad1
DennisHsieh <denny0530@gmail.com>
2020-10-07 18:20:02 +0800

    Constructed comment added

---
+++ funFind.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  // define SIZE = 20
+  // Give all value from 1 to 10 randomlly to this SIZE of data.
+  // There are many number from 1 to 10, choose which number you want to find out and output them.
+}
