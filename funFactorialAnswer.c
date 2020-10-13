e16573ec81a4914a378d10096ea21bb171f6aa6d
DennisHsieh <denny0530@gmail.com>
2020-10-13 18:21:22 +0800

    Done for input.!

--- funFactorialAnswer.c
+++ funFactorialAnswer.c
@@ -2,11 +2,13 @@
 #include<stdlib.h>
 int main(void){
 	short a[200];
-	int m=0,k,i,carry=0;
+	int m=0,k,i,carry=0,n;
+	printf("Input your number n:=");
+  scanf("%d",&n);
 	for(i=0;i<200;i++)
 	    a[i]=0;
 	a[m]=1;
-	for(k=2;k<=50;k++){
+	for(k=2;k<=n;k++){
 		for(i=0;i<=m;i++){
 			a[i]=a[i]*k;
 			a[i]=a[i]+carry;
@@ -15,9 +17,9 @@ int main(void){
 			a[i]=a[i]%10;
 		}
 	}
-	printf("50!=");
+	printf("%d! = ",n);
 	for(i=m;i>=0;i--)
 	    printf("%d",a[i]);
 	printf("\n");
-	system("pause");
+	// system("pause");
 }
