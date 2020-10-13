80f834192f46d40cc94f150ec44fdd57d7b266ac
DennisHsieh <denny0530@gmail.com>
2020-10-13 16:42:19 +0800

    Done, but need optimize output digit method

--- funFactorial.c
+++ funFactorial.c
@@ -7,12 +7,15 @@ int main(){
   // Use the code we write before Factorial.h
   printf("Input your number n:=");
   scanf("%ld",&n);
-  if (n<20){
+  if (n<=20){
     printf("%ld! = %ld\n",n,Factorial(n));
   }
   else {
     long Factorial20=Factorial(20),tmp,index[SIZE];
-    int i=0,j;
+    int i=0,DIGIT,j;
+    for(int i=0;i<SIZE;i++){
+      index[i]=0;
+    }
     // Store all Factorial20 to the array
     tmp=Factorial20;
     while (tmp > 0){
@@ -20,21 +23,29 @@ int main(){
       tmp=tmp/10;
       // printf("index[%d] = %ld , tmp = %ld\n",i,index[i],tmp);
       i++;
-      j=i;
+      DIGIT=i;
       // Error exception
       if (i >= SIZE){
         printf("SIZE is not large enough\n");
       }
     }
+    // Handling input value larger than 20
 
-    // Print to verify
+    for (int k=21;k<=n;k++){
+      int carry=0,tmp=0,i=0;
+      // while (i<=DIGIT || carry==0){
+      while (i<=100){
+        tmp=index[i];
+        index[i]=(k*index[i]+carry)%10;
+        carry=(k*tmp+carry)/10;
+        i++;
+        // j=i;
+      }
+    }
+    // Print to verify 20!
     printf("In index view is like \n");
-    for (int i=j-1;i>=0;i--){
+    for (int i=100;i>=0;i--){
       printf("%ld",index[i]);
     }
   }
-    // // Handling input value larger than 20
-    // for (int i=21;i<=n;i++){
-    //   i*Factorial20;
-    // }
 }
