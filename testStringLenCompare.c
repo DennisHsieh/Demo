16e87894f0d8ae137ba25f785868c9c004746368
DennisHsieh <denny0530@gmail.com>
2020-10-26 17:45:07 +0800

    Done simple implement for strncmp but in a foolish way to repeat many ocode

--- testStringLenCompare.c
+++ testStringLenCompare.c
@@ -4,10 +4,13 @@ int main(){
   char * name1 = "Matt Damon";
   char name2[] = "Dwayne Johnson";
   char name3[12] = "Matt Boomer";
-  // char namelist[0][3];
-  // namelist[0][0]=name1;
-  // namelist[0][0]=name1;
-  // namelist[0][0]=name1;
+  char namelist[3];
+  // namelist[0]=*name1;
+  // namelist[1]=*name2;
+  // namelist[2]=*name3;
+  // for (int i=0; i<3 ; i++){
+  //   printf("namelist[%d] = %s\n",i,namelist[i]); // Facing problmes due to lack of knowledge about string and character;
+  // }
   printf("name1 is %s with %ld characters\n",name1,strlen(name1));
   printf("name2 is %s with %ld characters\n",name2,strlen(name2));
   printf("name3 is %s with %ld characters\n",name3,strlen(name3));
@@ -16,5 +19,23 @@ int main(){
   }
   else {
     printf("%s != %s\n",name1,name2);
+  }
+  if (strncmp(name1,name3,20) == 0){
+    printf("%s == %s\n",name1,name3);
+  }
+  else {
+    printf("%s != %s\n",name1,name3);
+  }
+  if (strncmp(name1,name3,5) == 0){
+    printf("%s == %s when strncmp's n is set to be 5\n",name1,name3);
+  }
+  else {
+    printf("%s != %s\n",name1,name3);
+  }
+  if (strncmp(name2,name3,20) == 0){
+    printf("%s == %s\n",name2,name3);
+  }
+  else {
+    printf("%s != %s\n",name2,name3);
   }
 }
