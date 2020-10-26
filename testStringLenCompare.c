9c1f06c89b97c5b4116af296218bb76e52c00155
DennisHsieh <denny0530@gmail.com>
2020-10-26 18:10:44 +0800

    Optimize function call strncmpCustomize

--- testStringLenCompare.c
+++ testStringLenCompare.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+void strncmpCustomize(char* input1, char* input2, int n){
+  if (strncmp(input1,input2,n)!=0 ){
+    printf("%s != %s\n",input1,input2);
+  }
+  else if (strncmp(input1,input2,n)==0){
+    printf("\nIn strncmp(input1,input2,%d)\n",n);
+    printf("%s == %s \n\n",input1,input2);
+  }
+  else {
+    printf("Exception occur!\n");
+  }
+}
 int main(){
   char * name1 = "Matt Damon";
   char name2[] = "Dwayne Johnson";
@@ -14,28 +26,10 @@ int main(){
   printf("name1 is %s with %ld characters\n",name1,strlen(name1));
   printf("name2 is %s with %ld characters\n",name2,strlen(name2));
   printf("name3 is %s with %ld characters\n",name3,strlen(name3));
-  if (strncmp(name1,name2,20) == 0){
-    printf("%s == %s\n",name1,name2);
-  }
-  else {
-    printf("%s != %s\n",name1,name2);
-  }
-  if (strncmp(name1,name3,20) == 0){
-    printf("%s == %s\n",name1,name3);
-  }
-  else {
-    printf("%s != %s\n",name1,name3);
-  }
-  if (strncmp(name1,name3,5) == 0){
-    printf("%s == %s when strncmp's n is set to be 5\n",name1,name3);
-  }
-  else {
-    printf("%s != %s\n",name1,name3);
-  }
-  if (strncmp(name2,name3,20) == 0){
-    printf("%s == %s\n",name2,name3);
-  }
-  else {
-    printf("%s != %s\n",name2,name3);
-  }
+  strncmpCustomize(name1,name2,20);
+  strncmpCustomize(name2,name3,20);
+  strncmpCustomize(name1,name3,20);
+  strncmpCustomize(name1,name3,4);
+  strncmpCustomize(name1,name3,5);
+  strncmpCustomize(name1,name3,6);
 }
